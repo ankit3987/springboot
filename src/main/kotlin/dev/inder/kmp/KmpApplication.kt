0668243971b897930990913998b82f03802bbe50
Inder Bagga <er.inderbagga@gmail.com>
2024-07-05 16:54:50 +0530

    Initial commit.

---
+++ src/main/kotlin/dev/inder/kmp/KmpApplication.kt
@@ -0,0 +1,21 @@
+package dev.inder.kmp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class KmpApplication
+
+fun main(args: Array<String>) {
+	runApplication<KmpApplication>(*args)
+}
+
+@RestController
+class MessageController {
+
+	@GetMapping("/")
+	fun index(@RequestParam("name") name: String) = "Hello, $name!"
+}
---
+++ src/test/kotlin/dev/inder/kmp/KmpApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.inder.kmp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KmpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
